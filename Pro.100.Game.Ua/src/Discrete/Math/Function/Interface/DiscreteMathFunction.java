4265b822a2994ac8ad9e10f6a43029acd269c530
Nazar Danulyak <Nazar Danulyak>
2016-12-25 19:24:31 +0200

    Math

---
+++ Pro.100.Game.Ua/src/Discrete/Math/Function/Interface/DiscreteMathFunction.java
@@ -0,0 +1,7 @@
+package Discrete.Math.Function.Interface;
+
+import java.util.HashSet;
+
+public interface DiscreteMathFunction {
+	public HashSet<Integer> funclion(HashSet<Integer> A,HashSet<Integer> B);
+}
---
+++ Pro.100.Game.Ua/target/m2e-wtp/web-resources/META-INF/Discrete/Math/Function/Class/DiscreteMathAssociation.java
@@ -0,0 +1,19 @@
+package Discrete.Math.Function.Class;
+
+import java.util.HashSet;
+
+import Discrete.Math.Function.Interface.DiscreteMathFunction;
+
+//обєднання
+public class DiscreteMathAssociation implements DiscreteMathFunction {
+
+	@Override
+	public HashSet<Integer> funclion(HashSet<Integer> A, HashSet<Integer> B) {
+		HashSet<Integer> space = new HashSet<Integer>();
+		space.addAll(A);
+		space.addAll(B);
+		return space;
+
+	}
+
+}
