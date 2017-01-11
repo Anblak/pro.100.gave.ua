3789337af7598f94f22b38131a8b3b6ac3794991
danul <danul@DESKTOP-0MKJDAG>
2017-01-11 21:42:55 +0200

    add CreatorJS

---
+++ Pro.100.Game.Ua/src/main/java/resources/creatorJScript/CreateJScript.java
@@ -0,0 +1,62 @@
+package resources.creatorJScript;
+
+/**
+ * 
+ * @author prometej
+ *
+ */
+public class CreateJScript {
+	/**
+	 * 
+	 * @param input
+	 *            - input String JScript
+	 * @return
+	 * 
+	 */
+	public String JS(String input) {
+		return "<script>" + input + "</script>";
+	}
+
+	/**
+	 * 
+	 * @param command:
+	 *            document."command"
+	 * @return doucment.command
+	 */
+	public String Document(String command) {
+		return "document." + command;
+	}
+
+	/**
+	 * 
+	 * @param id
+	 *            - id element
+	 * @param command
+	 *            - getElementById('" + id + "').command
+	 * @return
+	 */
+	public String ByGetElement(String id, String command) {
+		return "getElementById('" + id + "')." + command;
+	}
+
+	/**
+	 * 
+	 * @param input
+	 *            - innerHTML = input
+	 * @return "innerHTML=" + input
+	 */
+	public String innerHTML(String input) {
+		return "innerHTML=" + input;
+	}
+
+	/**
+	 * 
+	 * @param input
+	 *            - innerHTML = input
+	 * @return "innerHTML+=" + input
+	 */
+	public String innerHTMLadd(String input) {
+		return "innerHTML+=" + input;
+	}
+
+}
