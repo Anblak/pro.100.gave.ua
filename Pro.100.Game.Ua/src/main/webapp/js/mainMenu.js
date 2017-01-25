53a9c8c1c3dd18d0b13b7eb8edba2dfe70455c18
danul <danul@DESKTOP-0MKJDAG>
2017-01-25 23:09:36 +0200

    fix

--- Pro.100.Game.Ua/src/main/webapp/js/mainMenu.js
+++ Pro.100.Game.Ua/src/main/webapp/js/mainMenu.js
@@ -3,6 +3,9 @@ function menuLeftOpen() {
 	document.getElementById('leftIndecatorMenu').style.left = "300px";
 	document.getElementById('leftIndecatorMenu').style.width = "100%";
 	document.getElementById('leftIndecatorMenu').style.opacity = "0.5";
+	document.getElementById('audio').style.display = 'none';
+	document.getElementById('userSettings').style.display = 'none';
+	document.getElementById('savephoto').style.display = 'none';
 }
 function menuLeftClose() {
 	document.getElementById('mainMenu').style.left = "-300px";
